454f3cb553e19437a690a7945ab0362b56231969
Kiran Gadamsetty <gadamsettykiran514@gmail.com>
2024-10-22 07:37:23 +0530

    adding Files

---
+++ index.js
@@ -0,0 +1,83 @@
+
+AOS.init();
+
+let element = document.getElementById("typewriter");
+let names = "based In India.";
+let i = 0;
+let uniqueNo = null
+let value = ""
+
+uniqueNo = setInterval(()=>{
+   if(i === names.length-1){
+    clearInterval(uniqueNo)
+   }
+   value = value + names[i]
+  element.textContent  = value
+  i++;
+}, 150)
+
+
+    
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    let container = document.querySelector(".experience-bottom-section");
+     let projects = document.getElementById("total-projects")
+     let experience = document.getElementById("total-experience");
+     let coding = document.getElementById("total-coding");
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                let projectsnumber = 0;
+                let experiencenumber = -10;
+                let codingNumber = 500;
+                let projectsNumberValue = null
+                let experienceNumberValue = null
+                let codingNumberValue = null
+                
+
+                projectsNumberValue = setInterval(()=>{
+                    if(projectsnumber === 30){
+                        clearInterval(projectsNumberValue)
+                    }
+                 projects.textContent = `${projectsnumber}+`
+                 projectsnumber++;
+                },50)
+               
+                experienceNumberValue = setInterval(()=>{
+                    if(experiencenumber === 2){
+                        clearInterval(experienceNumberValue)
+                    }
+                 experience.textContent = `${experiencenumber}+`
+                 experiencenumber++;
+                },50)
+                
+                codingNumberValue = setInterval(()=>{
+                    if(codingNumber === 1000){
+                        clearInterval(codingNumberValue)
+                    }
+                 coding.textContent = `${codingNumber}+`
+                 codingNumber++;
+                })
+
+
+
+
+
+
+
+
+                observer.unobserve(entry.target); 
+            }
+        });
+    });
+
+    observer.observe(container);
+});
+
+
+
+
+
